d5f2d973ab0321555c405efd6d45a542a95d6865
dimitri.tiago <dimitri.tiago@gmail.com>
2013-03-12 20:09:11 -0400

    Finalized polarity detection Finalized win block detection

--- COMP6721 AI/src/polarizedladder/WinPatternStrategy.java
+++ COMP6721 AI/src/polarizedladder/WinPatternStrategy.java
@@ -33,26 +33,25 @@ public class WinPatternStrategy
 			{new Point (-2,-1), new Point (-1,-1), new Point (-1,0), new Point(0,0), new Point(0,1)},
 			{new Point (-2,-2), new Point (-1,-2), new Point (-1,-1), new Point(0,-1), new Point(0,0)},
 			{new Point(0,0), new Point(-1,0), new Point (-1,1), new Point (-2,1) , new Point (-2,2)},		//	left ladder patterns
-			{new Point(0,0), new Point(1,0), new Point (0,1), new Point (-1,1) , new Point (-1,2)},
-			{new Point(0,0), new Point(-1,0), new Point (-1,1), new Point (0,-1) , new Point (1,-1)},
-			{new Point(0,0), new Point(0,1), new Point (1,0), new Point (1,-1) , new Point (2,-1)},
-			{new Point(0,0), new Point(0,-1), new Point (1,-1), new Point (1,-2) , new Point (2,-2)},
+			{new Point(1,0), new Point(0,0), new Point (0,1), new Point (-1,1) , new Point (-1,2)},
+			{new Point (1,-1), new Point (0,-1), new Point(0,0), new Point(-1,0), new Point (-1,1)},
+			{new Point (2,-1), new Point (1,-1), new Point (1,0), new Point(0,0), new Point(0,1)},
+			{new Point (2,-2),  new Point (1,-2), new Point (1,-1), new Point(0,-1), new Point(0,0)},
 	};
 	
-	private Point rightSidePolarity[] = { new Point(1,1), new Point(2,2), new Point(3,3), 
+	private Point leftSidePolarity[] = { new Point(1,1), new Point(2,2), new Point(3,3), 
 			new Point(4,4), new Point(5,5), new Point(6,6), new Point(7,7)};
 	
-	private Point leftSidePolarity[] = { new Point(1,13), new Point(2,12), new Point(3,11), 
-			new Point(4,10), new Point(5,9), new Point(6,8), new Point(7,7)};
+	private Point rightSidePolarity[] = { new Point(13,1), new Point(12,2), new Point(11,3), 
+			new Point(10,4), new Point(9,5), new Point(8,6), new Point(7,7)};
 	
 	public WinPatternStrategy(String[][] board) 
 	{
 		this.board = board;
 	}
 	
-	//TODO: check polarity of ladder.
 	//TODO: check for win block positions.
-	public boolean detectRightLadder(String playerToken, Point currPoint)
+	public boolean detectLadder(String playerToken, String opposingPlayerToken, Point currPoint)
 	{
 		String token = " ";
 		
@@ -106,7 +105,7 @@ public class WinPatternStrategy
 				else
 				{
 					// ladder not polarized, if no block, then win
-					return isWinBlocked(ladder, ladderDirection) ? false : true;
+					return isWinBlocked(ladder, ladderDirection, opposingPlayerToken) ? false : true;
 				}
 			}
 		}
@@ -114,7 +113,6 @@ public class WinPatternStrategy
 		return false;
 	}
 	
-	//TODO: optimize by removing points that are not worth checking
 	public boolean isPolarized(Point[] ladder, String ladderDirection)
 	{
 		// check polarity
@@ -122,33 +120,29 @@ public class WinPatternStrategy
 		{
 			return true;
 		}
-		else if (ladderDirection.equalsIgnoreCase("RIGHT")) // left side
+		else if (ladderDirection.equalsIgnoreCase("RIGHT")) // left polarity
 		{
-			for (int i = 0; i < ladder.length; i++)
+			for (int j = 0; j < (leftSidePolarity.length - 1); j++)
 			{
-				for (int j = 0; j < leftSidePolarity.length; j++)
+				if ( ladder[0].equals(leftSidePolarity[j])
+						&& ladder[2].equals(leftSidePolarity[j+1])
+						&& ladder[4].equals(leftSidePolarity[j+2]) )
 				{
-					if ( ladder[i].equals(leftSidePolarity[j])
-							&& ladder[i+1].equals(leftSidePolarity[j+1])
-							&& ladder[i+2].equals(leftSidePolarity[j+2]) )
-					{
-						return true;
-					}
+					System.out.println("Right Ladder Polarized!");	
+					return true;
 				}
 			}
 		}	
-		else if (ladderDirection.equalsIgnoreCase("LEFT")) 	// right side
+		else if (ladderDirection.equalsIgnoreCase("LEFT")) 	// right polarity
 		{
-			for (int i = 0; i < ladder.length; i++)
+			for (int j = 0; j < (rightSidePolarity.length - 1); j++)
 			{
-				for (int j = 0; j < rightSidePolarity.length; j++)
+				if ( ladder[0].equals(rightSidePolarity[j])
+						&& ladder[2].equals(rightSidePolarity[j+1])
+						&& ladder[4].equals(rightSidePolarity[j+2]) )
 				{
-					if ( ladder[i].equals(rightSidePolarity[j])
-							&& ladder[i+1].equals(rightSidePolarity[j+1])
-							&& ladder[i+2].equals(rightSidePolarity[j+2]) )
-					{
-						return true;
-					}
+					System.out.println("Left Ladder Polarized!");					
+					return true;
 				}
 			}
 		}
@@ -156,16 +150,35 @@ public class WinPatternStrategy
 		return false;
 	}
 	
-	public boolean isWinBlocked(Point[] ladder, String ladderDirection)
+	public boolean isWinBlocked(Point[] ladder, String ladderDirection, String opposingPlayerToken)
 	{
 		if ( ladderDirection.equalsIgnoreCase("LEFT") )
 		{
-		
+			Point p1 = new Point(ladder[0].x - 2, ladder[0].y);	// block point 1
+			Point p2 = new Point(ladder[0].x, ladder[0].y + 2);	// block point 2
+			
+			// if board points p1 and p2 do not contain player token...
+			if ( (board[p1.y][p1.x].equalsIgnoreCase(opposingPlayerToken)) &&
+					(board[p2.y][p2.x].equalsIgnoreCase(opposingPlayerToken)))
+			{
+				// win blocked
+				System.out.println("Left Win Blocked!");
+				return true;
+			}
 		}
 		else if ( ladderDirection.equalsIgnoreCase("RIGHT") )
 		{
+			Point p1 = new Point(ladder[0].x + 2, ladder[0].y);	// block point 1
+			Point p2 = new Point(ladder[0].x, ladder[0].y + 2);	// block point 2
 			
-			
+			// if board points p1 and p2 do not contain player token...
+			if ( (board[p1.y][p1.x].equalsIgnoreCase(opposingPlayerToken)) &&
+					(board[p2.y][p2.x].equalsIgnoreCase(opposingPlayerToken)))
+			{
+				// win blocked
+				System.out.println("Right Win Blocked!");
+				return true;
+			}
 		}
 		
 		return false;
@@ -183,18 +196,53 @@ public class WinPatternStrategy
 		myBoard.setPosition(3, 6, "*");
 		*/
 		
-		// left ladder test
+		/* left ladder test
 		myBoard.setPosition(1, 5, "*");
 		myBoard.setPosition(1, 4, "*");
 		myBoard.setPosition(2, 4, "*");
 		myBoard.setPosition(2, 3, "*");
 		myBoard.setPosition(3, 3, "*");
+		*/
 		
+		/* polarity test right ladder
+		myBoard.setPosition(2, 2, "*");
+		myBoard.setPosition(2, 3, "*");
+		myBoard.setPosition(3, 3, "*");
+		myBoard.setPosition(3, 4, "*");
+		myBoard.setPosition(4, 4, "*");
+		*/
+		
+		/* polarity test left ladder
+		myBoard.setPosition(7, 7, "*");
+		myBoard.setPosition(6, 7, "*");
+		myBoard.setPosition(6, 8, "*");
+		myBoard.setPosition(5, 8, "*");
+		myBoard.setPosition(5, 9, "*");
+		*/
+		
+		/* win left ladder blocked
+		myBoard.setPosition(2, 7, "*");
+		myBoard.setPosition(2, 6, "*");
+		myBoard.setPosition(3, 6, "*");
+		myBoard.setPosition(3, 5, "*");
+		myBoard.setPosition(4, 5, "*");
+		myBoard.setPosition(2, 5, "o");
+		myBoard.setPosition(4, 7, "o");
+		*/
+		
+		// win right ladder blocked
+		myBoard.setPosition(3, 7, "*");
+		myBoard.setPosition(3, 8, "*");
+		myBoard.setPosition(4, 8, "*");
+		myBoard.setPosition(4, 9, "*");
+		myBoard.setPosition(5, 9, "*");
+		myBoard.setPosition(5, 7, "o");
+		myBoard.setPosition(3, 9, "o");
 		
 		myBoard.printBoard();
 		
 		WinPatternStrategy detectWin = new WinPatternStrategy( myBoard.getState() );
-		boolean win = detectWin.detectRightLadder("*", new Point(4, 2) );
+		boolean win = detectWin.detectLadder("*", "o", new Point(8, 4) );
 		
 		System.out.printf("Win detected? %b\n", win);
 	}
